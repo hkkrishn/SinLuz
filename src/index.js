a31316cb8e9235735d7a49c3c7e7ba9cc177d1af
Harikrishnan <hkkrishn563@gmail.com>
2019-04-14 16:11:25 -0700

    fix eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainContent from "./components/MainContainer";
 import "./styles/main.scss";
-import * as serviceWorker from "./serviceWorker";
 import {createStore, applyMiddleware} from 'redux'
 import { Provider } from "react-redux";
 
@@ -17,7 +16,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
